3560a3e558006fc8fa0a858f7d47459ae4162f90
Sara Silva <ssilvag24@fpcoruna.afundacion.org>
2025-10-24 13:24:20 +0200

    Ejercicio 2

--- sprint1tkinter/ejercicio02.py
+++ sprint1tkinter/ejercicio02.py
@@ -1,1 +1,26 @@
+import tkinter as tk
 
+# Crear la ventana principal
+root = tk.Tk()
+root.title("Ejercicio 2")
+root.geometry("300x200")
+
+
+def mensaje():
+    etiqueta.config(text="Este es el ejercicio 2")
+
+def cerrar():
+    root.quit()
+
+# Crear widgets
+etiqueta = tk.Label(root, text="")
+etiqueta.pack(pady=5)
+
+boton1 = tk.Button(root, text="Mostrar mensaje", command=mensaje)
+boton1.pack(pady=5)
+
+boton2 = tk.Button(root, text="Cerrar", command=cerrar)
+boton2.pack(pady=5)
+
+# Ejercutar el bucle principal
+root.mainloop()
