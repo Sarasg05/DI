99c6848c28666a6cf11c02dc2f59374aa4a7017a
Sara Silva <ssilvag24@fpcoruna.afundacion.org>
2025-10-27 11:06:20 +0100

    Ejercicio 3

--- sprint1tkinter/ejercicio03.py
+++ sprint1tkinter/ejercicio03.py
@@ -1,1 +1,23 @@
+import tkinter as tk
 
+# Crear la ventana principal
+root = tk.Tk()
+root.title("Ejercicio 3")
+root.geometry("300x200")
+
+def saludar():
+    etiqueta.config(text=f"Hola, {entrada.get()}")
+
+
+#Crear widgets
+
+etiqueta = tk.Label(root, text="Escribe tu nombre: ")
+etiqueta.pack(pady=5)
+
+entrada = tk.Entry(root)
+entrada.pack(pady=5)
+
+boton = tk.Button(root, text="Saludar", command=saludar)
+boton.pack(pady=5)
+
+root.mainloop()
