2c4fc8139a58c927c3e9abbda9870d2e49507a4c
Sara Silva <ssilvag24@fpcoruna.afundacion.org>
2025-11-10 17:19:28 +0100

    Ejercicio 3

--- sprint3android/app/src/main/java/com/sarasilva/mycatalog/CatalogActivity.java
+++ sprint3android/app/src/main/java/com/sarasilva/mycatalog/CatalogActivity.java
@@ -1,6 +1,9 @@
 package com.sarasilva.mycatalog;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,5 +17,15 @@ public class CatalogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+
+        Button btnNavigateDetail = findViewById(R.id.btnNavigateDetail);
+        btnNavigateDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
