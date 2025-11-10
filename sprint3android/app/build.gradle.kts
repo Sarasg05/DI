0238fb59a08df7d4812062332d623a42a8ea459c
Sara Silva <ssilvag24@fpcoruna.afundacion.org>
2025-11-10 12:26:11 +0100

    Ejercicio 1

--- sprint3android/app/build.gradle.kts
+++ sprint3android/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.sarasilva.mycatalog"
-        minSdk = 28
+        minSdk = 25
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
